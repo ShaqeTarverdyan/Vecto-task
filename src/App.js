48d5dea8a27054dd8616f2a3934a1ce3db500ea6
ShaqeTarverdyan <tshaqe@gmail.com>
2023-11-16 15:18:24 +0400

    complete project adding all in App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback, useEffect } from 'react';
+import styled from 'styled-components';
+
+import Menu from './sections/menu';
+import FeaturedMovie from "./sections/featuredMovie";
+import TrendingSection from './sections/trendingSection';
+
+import movies from './data.json'
 
 function App() {
+  const [featuredMovie, setFeaturedMovie] = useState(movies.Featured);
+  const [lastSeenIds, setLastSeenIds] = useState([]);
+  const [storedLastSeenIds, setStoredLastSeenIds] = useState([]);
+
+
+  const limitedMovies = movies.TendingNow.length > 50 ? movies.TendingNow.slice(0, 50) : movies.TendingNow;
+  const updetedLimitedMovies = limitedMovies.map(movie => (
+    {...movie, lastSeenIndex: storedLastSeenIds.findIndex((id) =>  id === movie.Id)}
+
+  ))
+
+  updetedLimitedMovies.sort((a,b) => {
+    if (a.lastSeenIndex === b.lastSeenIndex){ 
+      if (Number(new Date(a.Date)) === Number(new Date(b.Date)))return 0;
+      else if (Number(new Date(a.Date) > Number(new Date(b.Date))))return -1;
+      else return 1
+    } else {
+      if (a.lastSeenIndex > b.lastSeenIndex) return -1;
+      else return 1;
+    }
+  });
+
+  const handleFeaturedMovieChange = useCallback((movie) => {
+    setLastSeenIds((prevLastSeenIds) =>  {
+      const newLastSeenIds = [...prevLastSeenIds];
+      const clickedMovieIndex = newLastSeenIds.findIndex((id) =>  id === movie.Id);
+      if(clickedMovieIndex !== -1) {
+        newLastSeenIds.splice(clickedMovieIndex, 1)
+      }
+      newLastSeenIds.push(movie.Id)
+      return newLastSeenIds;
+    });
+    setFeaturedMovie({...movie})
+  }, []);
+
+  useEffect(() => {
+    if(lastSeenIds.length) {
+      localStorage.setItem("lastSeenIds", JSON.stringify(lastSeenIds));
+    }
+
+  }, [lastSeenIds]);
+
+  useEffect(() => {
+    const stringLastSeenIds = localStorage.getItem("lastSeenIds");
+    const storedLastSeenIds = JSON.parse(stringLastSeenIds);
+    if(storedLastSeenIds) {
+      setLastSeenIds(storedLastSeenIds);
+      setStoredLastSeenIds(storedLastSeenIds);
+    }
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Menu/>
+      <Content>
+        <FeaturedMovie featuredMovie={featuredMovie}/>
+        <TrendingSection 
+          movies={updetedLimitedMovies} 
+          handleFeaturedMovieChange={handleFeaturedMovieChange}
+        />
+      </Content>
+    </Wrapper>
   );
 }
 
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const Content = styled.div`
+  position: ${({isOpenFullMenu}) => isOpenFullMenu ? 'relative' : 'unset'};
+  width: 100%;
+  height: 100vh;
+`;
 export default App;
